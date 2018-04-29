b67b00b915096be95925ef92721ae32465231c8d
Nehal Kanetkar <nkanetka@gmail.com>
2018-04-29 14:14:33 +0300

    Add methods and variables to match CLLocationManager

--- GpxLocationManager/LocationManager/LocationManaging.swift
+++ GpxLocationManager/LocationManager/LocationManaging.swift
@@ -15,22 +15,58 @@ enum LocationManagerType {
 }
 
 protocol LocationManaging {
-  var location: CLLocation { get }
-  var delegate: CLLocationManagerDelegate { get set }
-  var desiredAccuracy: CLLocationAccuracy { get set }
-  var activityType: CLActivityType { get set }
-  var distanceFilter: CLLocationDistance { get set }
+  var heading: CLHeading? { get }
+  var location: CLLocation? { get }
+
+  var delegate: CLLocationManagerDelegate? { get set }
+
   var pausesLocationUpdatesAutomatically: Bool { get set }
   var allowsBackgroundLocationUpdates: Bool { get set }
-  var monitoredRegions: Set<CLRegion> { get }
+  var showsBackgroundLocationIndicator: Bool { get set }
+  var distanceFilter: CLLocationDistance { get set }
+  var desiredAccuracy: CLLocationAccuracy { get set }
+  var activityType: CLActivityType { get set }
 
-  func authorizationStatus() -> CLAuthorizationStatus
+  var headingFilter: CLLocationDegrees { get set }
+  var headingOrientation: CLDeviceOrientation { get set }
+
+  var monitoredRegions: Set<CLRegion> { get }
+  var maximumRegionMonitoringDistance: CLLocationDistance { get }
+
+  var rangedRegions: Set<CLRegion> { get }
+
   func requestAlwaysAuthorization()
   func requestWhenInUseAuthorization()
+
+  func authorizationStatus() -> CLAuthorizationStatus
+  func locationServicesEnabled()
+  func deferredLocationUpdatesAvailable()
+  func significantLocationChangeMonitoringAvailable()
+  func headingAvailable()
+  func isMonitoringAvailable(for: AnyClass)
+  func isRangingAvailable()
+
   func startUpdatingLocation()
   func stopUpdatingLocation()
+  func requestLocation()
+
   func startMonitoringSignificantLocationChanges()
+  func stopMonitoringSignificantLocationChanges()
+
+  func startUpdatingHeading()
+  func stopUpdatingHeading()
+  func dismissHeadingCalibrationDisplay()
+
   func startMonitoring(for region: CLRegion)
   func stopMonitoring(for region: CLRegion)
+
+  func startRangingBeacons(in: CLBeaconRegion)
+  func stopRangingBeacons(in: CLBeaconRegion)
+  func requestState(for: CLRegion)
+
+  func startMonitoringVisits()
+  func stopMonitoringVisits()
+
   func disallowDeferredLocationUpdates()
+  func allowDeferredLocationUpdates(untilTraveled: CLLocationDistance, timeout: TimeInterval)
 }
